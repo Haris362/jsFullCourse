bea61ee81cd0036e4a8cfb0ccf74f09707b28fc4
Haris362 <haris.tariq423@gmail.com>
2025-06-27 02:52:45 +0500

    changes in array methods

---
+++ switch/script.js
@@ -0,0 +1,43 @@
+// Switch example
+const day = "Thursday"
+
+switch (day) {
+    // Case 1
+    case "Monday":
+        console.log("Day is monday");
+        break;
+        
+    // Case 2
+    case "Tuesday":
+        console.log("Day is tuesday");
+        break;
+        
+    // Case 3
+    case "Wednesday":
+        console.log("Day is wednesday");
+        break;
+        
+    // Case 4
+    case "Thursday":
+        console.log("Day is thursday");
+        break;
+    
+    // Case 5
+    case "Friday":
+        console.log("Day is friday");
+        break;
+
+    // Case 6
+    case "Saturday":
+        console.log("Day is saturday");
+        break;
+    
+    // Case 7    
+    case "Sunday":
+        console.log("Day is Sunday");
+        break;
+    
+    default:
+        console.log("Please entr the correct day");
+        break;
+}
---
+++ ternary_nullCoalescing-opreator/script.js
@@ -0,0 +1,17 @@
+// Ternary opreator
+const value = 30;
+const result =  value == 30 ? console.log("Value is equal to 30") : 
+console.log("Value is not equal to 30")
+
+const temp = 20;
+const output = temp >=40? console.log("Temperaure is hot"): console.log("Temperature is not hot") 
+
+
+//Nullish coalescing opretor
+const num = null;
+const result2 = num ?? console.log("Variable is null");
+
+const num2 = 12;
+const result3 = num2 ?? console.log("value is null");
+console.log(result3);
+
